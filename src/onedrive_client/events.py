6aa613b77b0ed63ef8a0427eff707d0ee57fc300
Yury <qualiapps@gmail.com>
2017-08-08 17:40:58 +0300

    OneDriveService: change events

--- src/onedrive_client/events.py
+++ src/onedrive_client/events.py
@@ -4,19 +4,11 @@ import dbus.glib
 
 
 class EventsHandler(object):
-    handlers = ('on_created', 'on_modified', 'on_deleted')
-
     def __init__(self, source):
-        for handler in self.handlers:
-            assert hasattr(source, handler)
-
         self.source = source
 
     def receive(self, data):
-        print "Processing data ..."
-        self.source.on_created(data)
-        self.source.on_modified(data)
-        self.source.on_deleted(data)
+        self.source.process(data)
 
 
 class DBusEvents(dbus.service.Object):
@@ -26,6 +18,12 @@ class DBusEvents(dbus.service.Object):
     def __init__(self):
         self.bn = dbus.service.BusName(DBusEvents.dbus_name, bus=dbus.SessionBus())
         super(DBusEvents, self).__init__(self.bn, DBusEvents.dbus_topic)
+
+    def process(self, data):
+        print "Processing data ..."
+        self.source.on_created(data)
+        self.source.on_modified(data)
+        self.source.on_deleted(data)
 
     @dbus.service.signal('onedrive.monitor.event')
     def on_created(self, data):
