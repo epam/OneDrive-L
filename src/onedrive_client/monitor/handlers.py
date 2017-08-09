7e4c894d4bfb420c06c5a16bc2b2fce4ca70023e
Yury Kavaliou <ykavaliou@ddn.com>
2017-08-09 12:50:40 +0300

    ODMonitor: invoke a proper event

--- src/onedrive_client/monitor/handlers.py
+++ src/onedrive_client/monitor/handlers.py
@@ -2,6 +2,9 @@ import dbus
 import dbus.service
 import dbus.glib
 
+from pprint import pprint
+from json import dumps
+
 
 class DataHandler(object):
     def __init__(self, source):
@@ -20,10 +23,17 @@ class DBusHandler(dbus.service.Object):
         super(DBusHandler, self).__init__(self.bn, DBusHandler.dbus_topic)
 
     def process(self, data):
-        print "Processing data ..."
-        self.on_created(data)
-        self.on_modified(data)
-        self.on_deleted(data)
+        print("Processing data ...")
+        for item in data:
+            # invokes a proper event
+            if hasattr(item, 'deleted') and item.deleted is not None:
+                event = self.on_deleted
+            elif item.created_date_time == item.last_modified_date_time:
+                event = self.on_created
+            else:
+                event = self.on_modified
+
+            event(dumps(item.to_dict()))
 
     @dbus.service.signal('onedrive.monitor.event')
     def on_created(self, data):
