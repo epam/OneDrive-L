f6655416ce094f22ecf798ea2b23c696a431e56e
Yury Kavaliou <ykavaliou@ddn.com>
2017-08-10 15:13:18 +0300

    ODMonitor: add doc strings

--- src/onedrive_client/onedrive/service.py
+++ src/onedrive_client/onedrive/service.py
@@ -13,33 +13,85 @@ class OneDriveService(object):
         self.client = client
 
     def set_root_path(self, name):
+        """
+        Set current directory
+        :param name: str, root dir
+        :return:
+        """
         self.root_path = name
 
     def list_drives(self):
+        """ Gets available drives
+        """
         pass
 
     def get_drive(self, drive_id):
+        """
+        Get metadata for a drive
+        :param drive_id:
+        :return:
+        """
         pass
 
     def get_item(self, item_id):
+        """
+        Get metadata for a OneDrive item
+        :param item_id:
+        :return:
+        """
         pass
 
     def copy_item(self, item_id, name=None):
+        """
+        Copy an Item on OneDrive
+        :param item_id:
+        :param name:
+        :return:
+        """
         pass
 
     def move_item(self, item_id, path):
+        """
+        Move an item on OneDrive
+        :param item_id:
+        :param path:
+        :return:
+        """
         pass
 
     def delete_item(self, item_id):
+        """
+        Delete an Item in OneDrive
+        :param item_id:
+        :return:
+        """
         pass
 
     def create_folder(self, parent_id, name):
+        """
+        Create a new folder in OneDrive
+        :param parent_id:
+        :param name:
+        :return:
+        """
         pass
 
     def download(self, item_id):
+        """
+        Download a OneDrive Item contents
+        :param item_id:
+        :return:
+        """
         pass
 
     def simple_upload(self, parent_id, name, content):
+        """
+        Simple item upload to OneDrive
+        :param parent_id:
+        :param name:
+        :param content:
+        :return:
+        """
         pass
 
     def get_changes(self):
