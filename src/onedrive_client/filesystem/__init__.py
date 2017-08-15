65de4c1aa8bafc1c16347f3543ab51dba881a34c
Ivan Ivanou <iivanou@ddn.com>
2017-08-15 15:51:00 +0300

    Filesystem the second

--- src/onedrive_client/filesystem/__init__.py
+++ src/onedrive_client/filesystem/__init__.py
@@ -1,3 +1,4 @@
 """
-Provides instruments for monitoring file system events and interacts with file system items
+Provides instruments for monitoring file system events and interacts
+with file system items
 """
