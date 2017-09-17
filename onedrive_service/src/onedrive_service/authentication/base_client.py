63f0d00edf9fa1266154c12c708192308df2c564
Aliaksei Buziuma <Aliaksei_Buziuma@epam.com>
2017-09-17 23:13:13 +0300

    Added auth fuctionality for OneDrive

---
+++ onedrive_service/src/onedrive_service/authentication/base_client.py
@@ -0,0 +1,16 @@
+import os
+import onedrivesdk
+
+
+class BaseOneDriveClient(onedrivesdk.OneDriveClient):
+    """ Base class for OneDrive API clients.
+    """
+
+    @staticmethod
+    def check_saved_session_exists() -> bool:
+        """ Returns 'True' if saved sessions exists.
+        """
+
+        current_path = os.getcwd()
+        session_path = os.path.join(current_path, 'session.pickle')
+        return os.path.exists(session_path)
---
+++ onedrive_service/src/onedrive_service/authentication/one_drive_client.py
@@ -0,0 +1,43 @@
+import onedrivesdk
+from onedrivesdk.helpers import GetAuthCodeServer
+
+from .session import Session
+from .base_client import BaseOneDriveClient
+
+
+API_BASE_URL = "https://api.onedrive.com/v1.0/"
+
+
+class OneDriveClient(BaseOneDriveClient):
+    """ API client for OneDrive for personal use.
+    """
+
+    def __init__(self, config: dict) -> None:
+        """ Basic initialization.
+        """
+
+        self._config = config
+        client_id = config["auth"]["client_id"]
+        scopes = config["auth"]["scopes"]
+
+        http_provider = onedrivesdk.HttpProvider()
+        auth_provider = onedrivesdk.AuthProvider(http_provider, client_id, scopes, session_type=Session)
+
+        super().__init__(API_BASE_URL, auth_provider, http_provider)
+
+    def authenticate(self, client_secret=None, redirect_uri="http://localhost:8080/", auth_code=None):
+        """ Authenticate process for OneDrive for personal use.
+        """
+
+        client_secret = client_secret if client_secret is not None else self._config["auth"]["client_secret"]
+
+        if self.check_saved_session_exists():
+            self.auth_provider.load_session()
+
+        else:
+            if auth_code is None:
+                auth_url = self.auth_provider.get_auth_url(redirect_uri)
+                auth_code = GetAuthCodeServer.get_auth_code(auth_url, redirect_uri)
+
+            self.auth_provider.authenticate(auth_code, redirect_uri, client_secret)
+            self.auth_provider.save_session()
